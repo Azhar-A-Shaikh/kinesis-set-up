045aaf756e98038a4e5516d030bbc4b20838d3d7
Azhar-A-Shaikh <117568791+Azhar-A-Shaikh@users.noreply.github.com>
2023-07-13 18:42:48 +0530

    Update customer_orders_raw_insert.py

--- customer_orders_raw_insert.py
+++ customer_orders_raw_insert.py
@@ -3,13 +3,13 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.amazon.aws.operators.redshift import RedshiftOperator
 import datetime
 
-SNOWFLAKE_CONN_ID = 'snowflake_conn'
+REDSHIFT_CONN_ID = 'redshift_conn'
 
 default_args = {
-    "owner": "snowflakedatapipelinepro",
+    "owner": "redshiftdatapipelinepro",
     "depends_on_past": False,
     "start_date": days_ago(1),
     "retries": 0,
@@ -28,87 +28,83 @@ bash_task = BashOperator(task_id="run_bash_echo", bash_command="echo 1", dag=dag
 
 post_task = BashOperator(task_id="post_dbt", bash_command="echo 0", dag=dag)
 
-batch_id =str(datetime.datetime.now().strftime("%Y%m%d%H%M"))
+batch_id = str(datetime.datetime.now().strftime("%Y%m%d%H%M"))
 print("BATCH_ID = " + batch_id)
 
-
 task_customer_landing_to_processing = BashOperator(
- task_id="customer_landing_to_processing",
- bash_command='aws s3 mv s3://snowflakedatapipelinepro/firehose/customers/landing/ s3://snowflakedatapipelinepro/firehose/customers/processing/{0}/ --recursive'.format(batch_id),
- dag=dag
+    task_id="customer_landing_to_processing",
+    bash_command='aws s3 mv s3://redshiftdatapipelinepro/firehose/customers/landing/ s3://redshiftdatapipelinepro/firehose/customers/processing/{0}/ --recursive'.format(
+        batch_id),
+    dag=dag
 )
- 
+
 task_customers_processing_to_processed = BashOperator(
- task_id="customer_processing_to_processed",
- bash_command='aws s3 mv s3://snowflakedatapipelinepro/firehose/customers/processing/{0}/ s3://snowflakedatapipelinepro/firehose/customers/processed/{0}/ --recursive'.format(batch_id),
- dag=dag
+    task_id="customer_processing_to_processed",
+    bash_command='aws s3 mv s3://redshiftdatapipelinepro/firehose/customers/processing/{0}/ s3://redshiftdatapipelinepro/firehose/customers/processed/{0}/ --recursive'.format(
+        batch_id),
+    dag=dag
 )
- 
+
 task_orders_landing_to_processing = BashOperator(
- task_id="orders_landing_to_processing",
- bash_command='aws s3 mv s3://snowflakedatapipelinepro/firehose/orders/landing/ s3://snowflakedatapipelinepro/firehose/orders/processing/{0}/ --recursive'.format(batch_id),
- dag=dag
+    task_id="orders_landing_to_processing",
+    bash_command='aws s3 mv s3://redshiftdatapipelinepro/firehose/orders/landing/ s3://redshiftdatapipelinepro/firehose/orders/processing/{0}/ --recursive'.format(
+        batch_id),
+    dag=dag
 )
- 
+
 task_orders_processing_to_processed = BashOperator(
- task_id="orders_processing_to_processed",
- bash_command='aws s3 mv s3://snowflakedatapipelinepro/firehose/orders/processing/{0}/ s3://snowflakedatapipelinepro/firehose/orders/processed/{0}/ --recursive'.format(batch_id),
- dag=dag
+    task_id="orders_processing_to_processed",
+    bash_command='aws s3 mv s3://redshiftdatapipelinepro/firehose/orders/processing/{0}/ s3://redshiftdatapipelinepro/firehose/orders/processed/{0}/ --recursive'.format(
+        batch_id),
+    dag=dag
 )
- 
-snowflake_query_orders = [
-    """copy into PRO_DB.PRO_SCHEMA.ORDERS_RAW
-(O_ORDERKEY,O_CUSTKEY,O_ORDERSTATUS,O_TOTALPRICE,O_ORDERDATE,O_ORDERPRIORITY,O_CLERK,O_SHIPPRIORITY,O_COMMENT, BATCH_ID) from
-( select t.$1,t.$2,t.$3,t.$4,t.$5,t.$6,t.$7,t.$8,t.$9,'{0}' from @ORDERS_RAW_STAGE t);""".format(batch_id),
+
+redshift_query_orders = [
+    """COPY PRO_SCHEMA.ORDERS_RAW (O_ORDERKEY,O_CUSTKEY,O_ORDERSTATUS,O_TOTALPRICE,O_ORDERDATE,O_ORDERPRIORITY,O_CLERK,O_SHIPPRIORITY,O_COMMENT, BATCH_ID)
+       FROM 's3://your-s3-bucket/orders_raw_stage_{0}'
+       IAM_ROLE 'your-redshift-iam-role'
+       FORMAT CSV
+       IGNOREHEADER 1;""".format(batch_id),
 ]
 
-snowflake_query_customers = [
-    """copy into PRO_DB.PRO_SCHEMA.CUSTOMER_RAW
-(C_CUSTKEY, C_NAME, C_ADDRESS, C_NATIONKEY, C_PHONE, C_ACCTBAL, C_MKTSEGMENT, C_COMMENT, BATCH_ID) from
-( select t.$1,t.$2,t.$3,t.$4,t.$5,t.$6,t.$7,t.$8,'{0}' from @CUSTOMER_RAW_STAGE t);""".format(batch_id),
+redshift_query_customers = [
+    """COPY PRO_SCHEMA.CUSTOMER_RAW (C_CUSTKEY, C_NAME, C_ADDRESS, C_NATIONKEY, C_PHONE, C_ACCTBAL, C_MKTSEGMENT, C_COMMENT, BATCH_ID)
+       FROM 's3://your-s3-bucket/customer_raw_stage_{0}'
+       IAM_ROLE 'your-redshift-iam-role'
+       FORMAT CSV
+       IGNOREHEADER 1;""".format(batch_id),
 ]
 
-
-snowflake_query_customer_orders_small_transformation = [
-    """insert into ORDER_CUSTOMER_DATE_PRICE (CUSTOMER_NAME, ORDER_DATE, ORDER_TOTAL_PRICE, BATCH_ID) 
-select c.c_name as customer_name, o.o_orderdate as order_date,sum(o.o_totalprice) as order_total_price, c.batch_id
-from orders_raw o join customer_raw c on o.o_custkey = c.C_custkey and o.batch_id = c.batch_id
-where o_orderstatus= 'F'
-group by c_name,o_orderdate, c.batch_id
-order by o_orderdate;""",
+redshift_query_customer_orders_small_transformation = [
+    """INSERT INTO PRO_SCHEMA.ORDER_CUSTOMER_DATE_PRICE (CUSTOMER_NAME, ORDER_DATE, ORDER_TOTAL_PRICE, BATCH_ID)
+       SELECT c.c_name AS customer_name, o.o_orderdate AS order_date, SUM(o.o_totalprice) AS order_total_price, c.batch_id
+       FROM PRO_SCHEMA.ORDERS_RAW o
+       JOIN PRO_SCHEMA.CUSTOMER_RAW c ON o.o_custkey = c.c_custkey AND o.batch_id = c.batch_id
+       WHERE o.o_orderstatus = 'F'
+       GROUP BY c.c_name, o.o_orderdate, c.batch_id
+       ORDER BY o.o_orderdate;""",
 ]
 
-snowflake_orders_sql_str = SnowflakeOperator(
-    task_id='snowflake_raw_insert_order',
+redshift_orders_sql_str = RedshiftOperator(
+    task_id='redshift_raw_insert_order',
     dag=dag,
-    snowflake_conn_id=SNOWFLAKE_CONN_ID,
-    sql=snowflake_query_orders,
-    warehouse="PRO_CURATION",
-    database="PRO_DB",
-    schema="PRO_SCHEMA",
-    role="PRO_DEVELOPER_ROLE",
+    redshift_conn_id=REDSHIFT_CONN_ID,
+    sql=redshift_query_orders,
 )
 
-snowflake_customers_sql_str = SnowflakeOperator(
-    task_id='snowflake_raw_insert_customers',
+redshift_customers_sql_str = RedshiftOperator(
+    task_id='redshift_raw_insert_customers',
     dag=dag,
-    snowflake_conn_id=SNOWFLAKE_CONN_ID,
-    sql=snowflake_query_customers,
-    warehouse="PRO_CURATION",
-    database="PRO_DB",
-    schema="PRO_SCHEMA",
-    role="PRO_DEVELOPER_ROLE",
+    redshift_conn_id=REDSHIFT_CONN_ID,
+    sql=redshift_query_customers,
 )
 
-snowflake_order_customers_small_transformation = SnowflakeOperator(
-    task_id='snowflake_order_customers_small_transformation',
+redshift_order_customers_small_transformation = RedshiftOperator(
+    task_id='redshift_order_customers_small_transformation',
     dag=dag,
-    snowflake_conn_id=SNOWFLAKE_CONN_ID,
-    sql=snowflake_query_customer_orders_small_transformation,
-    warehouse="PRO_CURATION",
-    database="PRO_DB",
-    schema="PRO_SCHEMA",
-    role="PRO_DEVELOPER_ROLE",
+    redshift_conn_id=REDSHIFT_CONN_ID,
+    sql=redshift_query_customer_orders_small_transformation,
 )
 
-[task_orders_landing_to_processing >> snowflake_orders_sql_str >> task_orders_processing_to_processed, task_customer_landing_to_processing >> snowflake_customers_sql_str >> task_customers_processing_to_processed] >> snowflake_order_customers_small_transformation >> post_task
+[task_orders_landing_to_processing >> redshift_orders_sql_str >> task_orders_processing_to_processed,
+ task_customer_landing_to_processing >> redshift_customers_sql_str >> task_customers_processing_to_processed] >> redshift_order_customers_small_transformation >> post_task
